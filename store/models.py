f623da9800fe487c63ab3b037e821011ac6d32fd
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-05 11:38:26 +0100

    Defining One To Many Relationship

--- store/models.py
+++ store/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerChoices()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 
 class Customer(models.Model):
@@ -43,9 +49,25 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=PAYMENT_STATUS_PENDING
     )
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity=models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
 
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
-    city= models.CharField(max_length=255)
-    customer=models.OneToOneField(Customer,on_delete=models.CASCADE,primary_key=True)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.PositiveSmallIntegerField()
