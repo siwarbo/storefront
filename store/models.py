731bfe14b43b3267ddc3f37fcf883a43825e9766
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-05 11:59:54 +0100

    Resolving Circular Relationships

--- store/models.py
+++ store/models.py
@@ -1,22 +1,26 @@
 from django.db import models
 
+
 class Promotion(models.Model):
-    description=models.CharField(max_length=255)
-    discount=models.FloatField()
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-
+    featured_product = models.ForeignKey(
+        "Product", on_delete=models.SET_NULL, null=True, related_name="+"
+    )
 
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerChoices()
+    inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions=models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
@@ -59,8 +63,8 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    quantity=models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Address(models.Model):
@@ -68,10 +72,12 @@ class Address(models.Model):
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
+
 class Cart(models.Model):
-    created_at=models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity=models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField()
