b156aee31380b53c1a94b543c68b9610577e3253
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-07 20:24:43 +0100

    Retreiving Objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 # Create your views here.
 def say_hello(request):
-    query_set = Product.objects.count()
+    exists = Product.objects.filter(pk=0).exists() 
     return render(request, "hello.html", {"name": "Siwar"})
