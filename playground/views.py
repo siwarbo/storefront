3a5e3d41159cfd1485d8c9e99b8475985666db1d
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-08 14:29:30 +0100

    Filtering Objects

--- playground/views.py
+++ playground/views.py
@@ -6,5 +6,5 @@ from store.models import Product
 
 # Create your views here.
 def say_hello(request):
-    exists = Product.objects.filter(pk=0).exists() 
-    return render(request, "hello.html", {"name": "Siwar"})
+    query_set = Product.objects.filter(title__icontains ="coffee")
+    return render(request, "hello.html", {"name": "Siwar", "products": list(query_set)})
