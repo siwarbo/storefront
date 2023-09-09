1f82dc7c0590c1e64746e68484684cf10d787533
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-09 12:57:40 +0100

    Aggregating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
+from django.db.models.aggregates import Count, Max, Min, Avg
+
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, OrderItem, Order
 
 
 # Create your views here.
 def say_hello(request):
-    query_set = (
-        Order.objects.select_related("customer")
-        .prefetch_related("orderitem_set__product")
-        .order_by("-placed_at")[:5]
-    )
-    return render(request, "hello.html", {"name": "Siwar", "orders": list(query_set)})
+    result = Product.objects.aggregate(count=Count("id"),minprice=Min('unit_price'))
+    return render(request, "hello.html", {"name": "Siwar", "result": result})
