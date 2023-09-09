1f3b32181d1b5c9dfcd8b2f5f8860e4f67be0e42
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-09 20:07:17 +0100

    Grouping Data

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q, F, Value, Func
+from django.db.models import Q, F, Value, Func,Count
 from django.db.models.functions import Concat
-from django.db.models.aggregates import Count, Max, Min, Avg
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, OrderItem, Order, Customer
 
@@ -10,10 +9,8 @@ from store.models import Product, OrderItem, Order, Customer
 # Create your views here.
 def say_hello(request):
     query_set = Customer.objects.annotate(
-        full_name=Func(F("first_name"), Value(" "), F("last_name"), function="CONCAT")
+        Orders_count=Count('order')
     )
-    query_set = Customer.objects.annotate(
-        full_name=Concat("first_name", Value(" "), "last_name")
-    )
+    
 
     return render(request, "hello.html", {"name": "Siwar", "result": list(query_set)})
