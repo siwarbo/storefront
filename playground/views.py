3cc20ee4d5687df8bde75fa76ffcc99eeb66d134
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-09 21:10:04 +0100

    Working With Exptrssion Wrappers

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q, F, Value, Func,Count
+from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper
 from django.db.models.functions import Concat
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, OrderItem, Order, Customer
+from django.db.models.fields import DecimalField
 
 
 # Create your views here.
 def say_hello(request):
-    query_set = Customer.objects.annotate(
-        Orders_count=Count('order')
+    discoounted_price = ExpressionWrapper(
+        F("unit_price") * 0.8, output_field=DecimalField()
     )
-    
+    query_set = Product.objects.annotate(discounted_price=discoounted_price)
 
     return render(request, "hello.html", {"name": "Siwar", "result": list(query_set)})
