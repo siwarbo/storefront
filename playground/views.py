687e2b7ebe98e2aeab4b434c45316d12358ca6c6
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-09 12:37:05 +0100

    Selecting Related Objects

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
-
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 
 # Create your views here.
 def say_hello(request):
-    query_set = Product.objects.defer("description")
-
-    return render(request, "hello.html", {"name": "Siwar", "products": list(query_set)})
+    query_set = (
+        Order.objects.select_related("customer")
+        .prefetch_related("orderitem_set__product")
+        .order_by("-placed_at")[:5]
+    )
+    return render(request, "hello.html", {"name": "Siwar", "orders": list(query_set)})
