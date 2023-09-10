04d40303d15c2e36807450479b83b38c408abba4
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-10 15:09:41 +0100

    Custom Managers

--- tags/models.py
+++ tags/models.py
@@ -4,11 +4,19 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 # Create your models here.
+class TaggedItemManager(models.Manager):
+    def get_taggs_for(self,object_type,object_id):
+        
+        content_type = ContentType.objects.get_for_model(object_type)
+        return TaggedItem.objects.select_related("tag").filter(
+        content_type=content_type, object_id=object_id
+        )
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
 class TaggedItem(models.Model):
+    objects=TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
--- tags/views.py
+++ tags/views.py
@@ -9,9 +9,6 @@ from tags.models import ContentType, TaggedItem
 
 # Create your views here.
 def say_hello(request):
-    content_type = ContentType.objects.get_for_model(Product)
-    query_set = TaggedItem.objects.select_related("tag").filter(
-        content_type=content_type, object_id=1
-    )
+    query_set = TaggedItem.objects.get_tags_for(Product, 1)
 
     return render(request, "hello.html", {"name": "Siwar", "tags": list(query_set)})
