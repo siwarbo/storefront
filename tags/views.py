9d22a74120f16df3883ddcb133f2f372ea87d4d5
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-10 15:37:52 +0100

    Creating Objects

--- tags/views.py
+++ tags/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper
 from django.db.models.functions import Concat
-from store.models import Product, OrderItem, Order, Customer
+from store.models import Product, OrderItem, Order, Customer, Collection
 from django.db.models.fields import DecimalField
 from tags.models import ContentType, TaggedItem
 
 
 # Create your views here.
 def say_hello(request):
-    query_set = Product.objects.all()
-    list(query_set)
-    query_set[0]
+    collection = Collection()
+    collection.title = "Video Games"
+    collection.featured_product = Product(pk=1)
+    collection.save()
+    collection.id
 
-    return render(request, "hello.html", {"name": "Siwar", "tags": list(query_set)})
+    return render(request, "hello.html", {"name": "Siwar"})
