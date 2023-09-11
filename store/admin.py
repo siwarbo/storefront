0be4b465dc1139bd9a315a88f632bab34889695a
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-11 12:53:38 +0100

    Adding Computed Columuns

--- store/admin.py
+++ store/admin.py
@@ -4,16 +4,22 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["title", "unit_price"]
+    list_display = ["title", "unit_price", "inventory_status"]
     list_editable = ["unit_price"]
     list_per_page = 10
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return "Low"
+        return "OK"
 
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "membership"]
     list_editable = ["membership"]
-    ordering = ["first_name","last_name"]
+    ordering = ["first_name", "last_name"]
     list_per_page = 10
 
 
