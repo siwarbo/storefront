6bd612ce3f7a0324660a508b1476edae8301785d
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-11 11:39:24 +0100

    Setting Up The Admin Site

--- storefront/urls.py
+++ storefront/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 import debug_toolbar
 
+admin.site.site_header = "Storefront Admin"
+admin.site.index_title = "Admin"
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')),
+    path("admin/", admin.site.urls),
+    path("playground/", include("playground.urls")),
     path("__debug__/", include(debug_toolbar.urls)),
 ]
