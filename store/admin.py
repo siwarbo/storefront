5c26561e197a0f9a29846931526cb342c208702a
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-11 21:05:49 +0100

    Overiding The Base QuerySet

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,8 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.db.models import Count
+from django.http.request import HttpRequest
 from . import models
 
 
@@ -29,6 +33,17 @@ class OrderAdmin(admin.ModelAdmin):
 
     def customer_id(self, order):
         return order.customer_id
+
+
+# @admin.register(models.Collection)
+# class CollectionAdmin(admin.ModelAdmin):
+#     list_display = ["title", "products_count"]
+
+#     def products_count(self, collection):
+#         return collection.products_count
+
+#     def get_queryset(self, request):
+#         return super().get_queryset(request).annotate(products_count=Count("product"))
 
 
 @admin.register(models.Customer)
