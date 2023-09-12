ab17cb586fd3a36c935a0159a69b109a4732cd91
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-12 18:46:35 +0100

    Fixing Collection Bug

--- store/admin.py
+++ store/admin.py
@@ -60,21 +60,21 @@ class OrderAdmin(admin.ModelAdmin):
         return order.customer_id
 
 
-# @admin.register(models.Collection)
-# class CollectionAdmin(admin.ModelAdmin):
-#     list_display = ["title", "products_count"]
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title", "products_count"]
 
-#     @admin.display(ordering="products_count")
-#     def products_count(self, collection):
-#         url = (
-#             reverse("admin:store_product_changelist")
-#             + "?"
-#             + urlencode({"collection__id": str(collection.id)})
-#         )
-#         return format_html('<a href="{}">{}</a>', url, collection.products_count)
+    @admin.display(ordering="products_count")
+    def products_count(self, collection):
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode({"collection__id": str(collection.id)})
+        )
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
 
-#     def get_queryset(self, request):
-#         return super().get_queryset(request).annotate(products_count=Count("product"))
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count=Count("product"))
 
 
 @admin.register(models.Customer)
@@ -84,6 +84,3 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ["first_name", "last_name"]
     list_per_page = 10
     search_fields = ["first_name__istartswith", "last_name__istartswith"]
-
-
-admin.site.register(models.Collection)
