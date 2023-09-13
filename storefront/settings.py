673cb8401ea011ecf24b7afa082849d635dd94dc
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-13 19:59:52 +0100

    Install Django REST Framework

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "playground",
     "debug_toolbar",
     "store",
@@ -92,7 +93,6 @@ DATABASES = {
         "HOST": "localhost",
         "USER": "root",
         "PASSWORD": "root",
-
     }
 }
 
