bf423211468776fb6f75f3ea5a471b10ca1d3654
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-15 11:02:08 +0100

    Desserializing Objects

--- store/views.py
+++ store/views.py
@@ -3,17 +3,20 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from store.serializers import ProductSerializer,CollectionSerializer
-from store.models import Product,Collection
+from store.serializers import ProductSerializer, CollectionSerializer
+from store.models import Product, Collection
 
 
 # Create your views here.
-@api_view()
+@api_view(['GET','POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(
-        queryset, many=True,context={'request':request} )
-    return Response(serializer.data)
+    if request.method=='GET':
+        queryset = Product.objects.select_related("collection").all()
+        serializer = ProductSerializer(queryset, many=True, context={"request": request})
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer=ProductSerializer(data=request.data)
+        return Response('ok')
 
 
 @api_view()
@@ -22,10 +25,10 @@ def product_detail(request, id):
     serializer = ProductSerializer(product)
     return Response(serializer.data)
 
+
 @api_view()
 def collection_detail(request, pk):
     # collection = get_object_or_404(Collection, pk=id)
     # serializer = CollectionSerializer(collection)
     # return Response(serializer.data)
-    return Response('ok')
-
+    return Response("ok")
