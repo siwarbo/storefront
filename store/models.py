9b8fd1c368ca468331b1593019b32f6b7c6182b5
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2023-09-20 11:58:44 +0100

    Adding Authentification Endpoint

--- store/models.py
+++ store/models.py
@@ -92,6 +92,9 @@ class Order(models.Model):
     )
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    class Meta:
+        permissions = [("cancel_order", "Can cancel order")]
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
--- storefront/settings.py
+++ storefront/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "djoser",
     "django_filters",
     "playground",
     "debug_toolbar",
@@ -141,6 +142,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+       
+    ),
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("JWT",),
 }
 
 
